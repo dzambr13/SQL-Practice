967d1bb703efc593de8df2e9ebcadb614fd4adcf
dzambr13 <dayvuhz@gmail.com>
2022-10-24 11:07:07 -0700

    specified returning

--- select.js
+++ select.js
@@ -1,10 +1,15 @@
-//returns all data from a table 
+//returns ALL data from a table 
 select * from player 
 
 //if you only want to select specific fields
-//with this commend, you return the all the players names and their respective birthdays
+//with this commend, you return the the players names and their respective birthdays
 select player_name, birthday from player 
 
 //changing the name with the "as" command
 select player_name as name, birthday from player
 
+//return specific rows in a table
+//this returns ALL players that weight 190, greater than, or greater than or = to 
+select * from player where weight=190
+select * from player where weight>190
+select * from player where weight>=190
