d453490f7c3d90eb13223c66f5cf99a1bb11bdf9
dzambr13 <dayvuhz@gmail.com>
2022-10-24 11:03:42 -0700

    select sql section

---
+++ select.js
@@ -0,0 +1,10 @@
+//returns all data from a table 
+select * from player 
+
+//if you only want to select specific fields
+//with this commend, you return the all the players names and their respective birthdays
+select player_name, birthday from player 
+
+//changing the name with the "as" command
+select player_name as name, birthday from player
+
