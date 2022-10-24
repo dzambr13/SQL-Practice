baed395a1dbc5b01573007cdda4b82e6d62412d7
dzambr13 <dayvuhz@gmail.com>
2022-10-24 14:08:49 -0700

    cleanup

--- select.js
+++ select.js
@@ -6,10 +6,18 @@ player
 
 //if you only want to select specific fields
 //with this commend, you return the the players names and their respective birthdays
-select player_name, birthday from player 
+select 
+player_name, 
+birthday 
+from 
+player 
 
 //changing the name with the "as" command
-select player_name as name, birthday from player
+select 
+player_name as name, 
+birthday 
+from 
+player
 
 //return specific rows in a table
 //this returns ALL players that weight 190, greater than, or greater than or = to 
