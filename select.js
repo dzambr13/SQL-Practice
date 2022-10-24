b51a3769b17bb62ab931015ade57dd1c1da01400
dzambr13 <dayvuhz@gmail.com>
2022-10-24 14:12:31 -0700

    since SQL isnt case sensitive, i just caps the key words, still doing clean up

--- select.js
+++ select.js
@@ -1,35 +1,58 @@
 //returns ALL data from a table 
-select 
+SELECT 
 * 
-from 
+FROM 
 player 
 
 //if you only want to select specific fields
 //with this commend, you return the the players names and their respective birthdays
-select 
+SELECT 
 player_name, 
 birthday 
-from 
+FROM 
 player 
 
 //changing the name with the "as" command
-select 
-player_name as name, 
+SELECT
+player_name as 'Full name', 
 birthday 
-from 
+FROM
 player
 
 //return specific rows in a table
 //this returns ALL players that weight 190, greater than, or greater than or = to 
-select * from player where weight=190
-select * from player where weight>190
-select * from player where weight>=190
+SELECT
+* 
+FROM
+player 
+WHERE weight=190
+
+SELECT 
+* 
+FROM
+player 
+WHERE weight>190
+
+SELECT 
+* 
+FROM
+player 
+WHERE weight>=190
 
 //"and" and "or" statements
 //"and" where both conditions have to be met
 //"or" where only one of the conditions have to be met
-select * from player where weight>190 and height>190
-select * from player where weight>190 or height>190
+select 
+* 
+from 
+player 
+where weight>190 AND height>190
+
+select 
+* 
+from 
+player 
+where weight>190 AND height>190
 
 //select for non integers 
 //must follow up with and equals and SINGLE QUOTES ex: ='Dave Zambrano'
