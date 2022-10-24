7e28375162e4fc12cc8af0076ddd80648162a603
dzambr13 <dayvuhz@gmail.com>
2022-10-24 16:13:16 -0700

    data types

---
+++ tables.js
@@ -0,0 +1,6 @@
+//data types:
+//int
+//decimal (s,d) (decimal ex: 6,2)
+//varchar(30) can contain letter, numbers, and special character, or all 3
+//date --formatted as YYYY-MM-DD
+//timestamp --formatted as YYYY-MM-DD HH:MM:SS
