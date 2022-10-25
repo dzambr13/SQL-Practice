1d17f4064b711ef3016a591ac1cc358fe8e61bc7
dzambr13 <dayvuhz@gmail.com>
2022-10-24 17:26:34 -0700

    trim syntax

---
+++ cleaningdata.js
@@ -0,0 +1,5 @@
+//learning to correct shit format or incorrect data
+//the format would be shit if it was copied from one place to another and it didn't format right
+//LTRIM(string), removes leading spaces
+//RTRIM(string), removes trailing spaces
+//TRIM(string), removes both leading and trailing
