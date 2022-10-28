09c7cd29859accce7d8401b58290c2dffc84fbb6
dzambr13 <dayvuhz@gmail.com>
2022-10-28 08:55:06 -0700

    list methods

---
+++ listmethod.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5]
+# numbers.append(6)
+# numbers.insert(0, -1)
+# numbers.remove(3)
+# numbers.clear()
+
+print(len(numbers))
+# returns how many elements are in the list of numbers
+
+# print(1 in numbers)
+# boolean value
