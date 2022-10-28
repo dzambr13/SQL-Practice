512afa89f7c228126d4dad37bfa1e7a1fe7d4ff7
dzambr13 <dayvuhz@gmail.com>
2022-10-28 09:10:15 -0700

    range

--- rangefx.py
+++ rangefx.py
@@ -1,5 +1,11 @@
-numbers = range(5)
+# won't print 10, the third number is the increment at which the numbers will print in
+numbers = range(5, 10, 2)
 for number in numbers:
     print(number)
 
 # in this code, we itterated through numbers using the for loop method
+
+for numberss in range(5):
+    print(numberss)
+
+# easier way to do it^^
