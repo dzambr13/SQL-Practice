4521925a7b2882b92336aab3100ade15eae9e5fb
dzambr13 <dayvuhz@gmail.com>
2022-10-27 22:54:33 -0700

    int fx

---
+++ app.py
@@ -0,0 +1,5 @@
+birth_year = input("Enter your birth year: ")
+age = 2022 - int(birth_year)
+print(age)
+
+# line 2 must be an integer so that 2022 can be subtracted properly, we did that by just setting birth_year to an int() function.
