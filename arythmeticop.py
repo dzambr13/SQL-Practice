b1e2b1ce559506cedd401ab6d9d3f7bfc2a5550d
dzambr13 <dayvuhz@gmail.com>
2022-10-28 08:11:40 -0700

    variables

--- arythmeticop.py
+++ arythmeticop.py
@@ -2,3 +2,6 @@ print(10 / 3)  # returns a float
 print(10 // 3)  # returns an int
 print(10 % 3)  # returns the remainder of 10/3
 print(10 ** 3)  # returns 10 to the power of 3
+x = 10
+x = x + 3  # indentical to this x += 3
+print(x)
