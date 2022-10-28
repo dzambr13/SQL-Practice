828771229381b213c87d4a160245a9c4dac3ffcd
dzambr13 <dayvuhz@gmail.com>
2022-10-27 23:07:23 -0700

    float calc

--- calcprogram.py
+++ calcprogram.py
@@ -1,6 +1,6 @@
-first = int(input("First: "))
-second = int(input("Second: "))
-sum = first + second
-print(sum)
+first = input("First: ")
+second = input("Second: ")
+sum = float(first) + float(second)
+print("Sum: " + str(sum))
 
-# calculator lol
+# line 4, need to convert string to string since python doesn't know how to concat floats or integers or whatever, only strings i guess
