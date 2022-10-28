ef9cbf14d82958c897b072ba7e74377e4abfd863
dzambr13 <dayvuhz@gmail.com>
2022-10-27 22:57:06 -0700

    some rules

--- inputfx.py
+++ inputfx.py
@@ -2,3 +2,7 @@ name = input("What is your name? ")
 print("Hello " + name)
 
 # when you run this, enter in your name in the terminal, then it'll return back, "Hello (name)"
+
+float()  # float is a decimal number
+bool()  # converts a value to a boolean
+str()  # converts a value to a string
