6788cff235cf8d2a3ec0945f3528372a2ae7b775
dzambr13 <dayvuhz@gmail.com>
2022-10-27 22:49:07 -0700

    inputfx

--- app.py
+++
@@ -1,5 +0,0 @@
-age = 20
-price = 19.95
-first_name = "Dave"
-is_online = False
-print(age, first_name)
---
+++ inputfx.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+print("Hello " + name)
+
+# when you run this, enter in your name in the terminal, then it'll return back, "Hello (name)"
