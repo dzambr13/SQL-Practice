a79943d923e10af53d0aa9b46198987daef1b232
dzambr13 <dayvuhz@gmail.com>
2022-10-28 08:26:15 -0700

    if

--- ifstate.py
+++ ifstate.py
@@ -1,5 +1,13 @@
-temperature = 35
+temperature = 25
 
 if temperature > 30:
     print("It's a hot day")
     print("Drink plent of water")
+elif temperature > 20:
+    # (20, 30], this means between 20 and greater or equal to 30
+    print("It's a nice day")
+elif temperature > 10:
+    print("It's a bit cold")
+else:
+    print("It's cold")
+print("Done")
