2e74decd494e097c437445637f7ce923f28137ba
dzambr13 <dayvuhz@gmail.com>
2022-10-27 23:17:38 -0700

    delcaring

--- declaring.py
+++ declaring.py
@@ -1,5 +1,6 @@
 course = 'Python for Beginners'
-print(course.replace('for', '4'))
-print(course.find('y'))
-print(course.upper())
-print(course.lower())
+print('Python' in course)
+# print(course.replace('for', '4'))
+# print(course.find('y'))
+# print(course.upper())
+# print(course.lower())
